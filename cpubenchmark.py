3cc7b6fbf64fbee2b578fe9a9b4e9f3fb2eaadc2
DarkAssassin23 <15916504+DarkAssassin23@users.noreply.github.com>
2023-08-16 18:57:18 -0400

    Removed unneeded code

--- cpubenchmark.py
+++ cpubenchmark.py
@@ -21,64 +21,6 @@ cpuDataDict = {
     "Threads":[]
     }
 
-# Checks to see if the CPU list file exists
-def validInputFile():
-    if(not os.path.exists(cpuListFileName)):
-        print("Error: Could not find the file specified.")
-        print("File \'"+cpuListFileName+"\' does not exist")
-        return False
-    return True
-
-# Gets a list of all the CPUs from the
-# list of CPUs file
-def getCPUs():
-    lines = ""
-    cpus = []
-    with open(cpuListFileName, 'r') as f:
-        lines = f.readlines()
-    for line in lines:
-        # Find where comments start and ignore everything
-        # after the start of the comment
-        if(line.find("#") != -1 or line.find("//") != -1):
-            if(min(line.find("#"), line.find("//")) != -1):
-                line = line[:min(line.find("#"), line.find("//"))]
-            else:
-                line = line[:max(line.find("#"), line.find("//"))]
-        if(not line.strip() == ''):
-            cpus.append(line.strip())
-    return cpus
-
-# Adds any auxiliary data to the respective cpu
-def addAuxData(currentData, cpuDict):
-    for k,v in currentData.items():
-        # If k is not in cpuDict it is auxiliary
-        if not k in cpuDict:
-            count = 0
-            cpuDict[k] = [None] * len(cpuDict["Name"])
-            for curName in cpuDict["Name"]:
-                if(curName in currentData["Name"]):
-                    cpuDict[k][count] = currentData[k][currentData["Name"].index(curName)]
-                else:
-                    cpuDict[k][count] = "N/A"
-
-                count += 1
-
-# If the output CSV file exists, it gets read in
-# so if you added additional info to the csv
-# that info will be transfered, if the cpu exists
-def readCSV():
-    d={}
-    if(os.path.exists(csvFileName)):
-        with open(csvFileName, 'r', newline='') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                for k, v in row.items():
-                    if not k in d:
-                        d[k] = []
-                    d[k].append(v)
-    return d
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Web scraper to pull CPU data from cpubenchmark.net')
     parser.add_argument('-o', nargs=1, metavar="file", help="output file to save data to")
